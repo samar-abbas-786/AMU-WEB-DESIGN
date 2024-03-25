6a17c43413bb10b6ee632e13e25a975415640292
Samar Abbas <samarabbas172003@gmail.com>
2024-03-25 05:41:32 +0530

    add principle

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# Academic Project
+
+### Introduction:
+
+"Welcome to my academic project, a culmination of my learning journey and practical application of web development skills."
+
+### Project Overview:
+
+"This project aims to demonstrate proficiency in front-end web development, showcasing CSS animations, and interactive user interfaces."
+
+### Design Inspiration:
+
+"Inspired by modern web design trends, this project incorporates sleek layouts, simple colors, and subtle animations to enhance user engagement."
---
+++ index.js
@@ -0,0 +1,169 @@
+function c1() {
+  // Prevent default link behavior
+
+  var url =
+    "https://api.amu.ac.in/storage//file/10001/file_management/1668069477.pdf";
+  var newTab = window.open(url, "_blank");
+  newTab.focus();
+}
+function c2() {
+  // Prevent default link behavior
+
+  var url =
+    "https://api.amu.ac.in/storage//file/10001/file_management/1669956486.pdf";
+  var newTab = window.open(url, "_blank");
+  newTab.focus();
+}
+function c3() {
+  // Prevent default link behavior
+
+  var url =
+    "https://api.amu.ac.in/storage//file/10001/file_management/1668072618.pdf";
+  var newTab = window.open(url, "_blank");
+  newTab.focus();
+}
+function c4() {
+  // Prevent default link behavior
+
+  var url =
+    "https://api.amu.ac.in/storage//file/10001/file_management/1668072731.pdf";
+  var newTab = window.open(url, "_blank");
+  newTab.focus();
+}
+function c5() {
+  // Prevent default link behavior
+
+  var url =
+    "https://api.amu.ac.in/storage//file/10001/file_management/1668072423.pdf";
+  var newTab = window.open(url, "_blank");
+  newTab.focus();
+}
+function c6() {
+  // Prevent default link behavior
+
+  var url =
+    "https://api.amu.ac.in/storage//file/10001/file_management/1668072343.pdf";
+  var newTab = window.open(url, "_blank");
+  newTab.focus();
+}
+function c7() {
+  // Prevent default link behavior
+
+  var url =
+    "https://api.amu.ac.in/storage//file/10001/file_management/1668068976.pdf";
+  var newTab = window.open(url, "_blank");
+  newTab.focus();
+}
+function c8() {
+  // Prevent default link behavior
+
+  var url =
+    "https://api.amu.ac.in/storage//file/10001/file_management/1668073140.pdf";
+  var newTab = window.open(url, "_blank");
+  newTab.focus();
+}
+function c9() {
+  // Prevent default link behavior
+
+  var url =
+    "https://api.amu.ac.in/storage//file/10001/file_management/1668068070.pdf";
+  var newTab = window.open(url, "_blank");
+  newTab.focus();
+}
+function c10() {
+  // Prevent default link behavior
+
+  var url =
+    "https://api.amu.ac.in/storage//file/10001/file_management/1668068133.pdf";
+  var newTab = window.open(url, "_blank");
+  newTab.focus();
+}
+function c11() {
+  // Prevent default link behavior
+
+  var url =
+    "https://api.amu.ac.in/storage//file/10001/file_management/1668067980.pdf";
+  var newTab = window.open(url, "_blank");
+  newTab.focus();
+}
+function c12() {
+  // Prevent default link behavior
+
+  var url =
+    "https://api.amu.ac.in/storage//file/10001/file_management/1668068783.pdf";
+  var newTab = window.open(url, "_blank");
+  newTab.focus();
+}
+function c13() {
+  // Prevent default link behavior
+
+  var url =
+    "https://api.amu.ac.in/storage//file/10001/file_management/1668068740.pdf";
+  var newTab = window.open(url, "_blank");
+  newTab.focus();
+}
+function c14() {
+  // Prevent default link behavior
+
+  var url =
+    "https://api.amu.ac.in/storage//file/10001/file_management/1668072172.pdf";
+  var newTab = window.open(url, "_blank");
+  newTab.focus();
+}
+function c15() {
+  // Prevent default link behavior
+
+  var url =
+    "https://api.amu.ac.in/storage//file/10001/file_management/1668072074.pdf";
+  var newTab = window.open(url, "_blank");
+  newTab.focus();
+}
+function c16() {
+  // Prevent default link behavior
+
+  var url =
+    "https://api.amu.ac.in/storage//file/10001/file_management/1668071821.pdf";
+  var newTab = window.open(url, "_blank");
+  newTab.focus();
+}
+function c17() {
+  // Prevent default link behavior
+
+  var url =
+    "https://api.amu.ac.in/storage//file/10001/file_management/1668071902.pdf";
+  var newTab = window.open(url, "_blank");
+  newTab.focus();
+}
+function c18() {
+  // Prevent default link behavior
+
+  var url =
+    "https://api.amu.ac.in/storage//file/10001/file_management/1668135896.pdf";
+  var newTab = window.open(url, "_blank");
+  newTab.focus();
+}
+function c19() {
+  // Prevent default link behavior
+
+  var url =
+    "https://api.amu.ac.in/storage//file/10001/file_management/1668071964.pdf";
+  var newTab = window.open(url, "_blank");
+  newTab.focus();
+}
+function c20() {
+  // Prevent default link behavior
+
+  var url =
+    "https://api.amu.ac.in/storage//file/10001/file_management/1668072012.pdf";
+  var newTab = window.open(url, "_blank");
+  newTab.focus();
+}
+
+function know() {
+  // Prevent default link behavior
+
+  var url =
+    "https://www.amu.ac.in/centres/centre-for-skill-development-and-career-planning";
+  var newTab = window.open(url, "_blank");
+  newTab.focus();
+}
